4d7fc08914de731cf1e5c6c5dc4b47aa51b11528
0xhatsume <0xhatsume@gmail.com>
2024-01-02 22:22:23 +0800

    :seedling: Seed bare bones monster tamer preload scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import React, { useState } from 'react';
 import './App.css';
+import Phaser from 'phaser';
+import { usePhaserGame } from './hooks/usePhaserGame';
+import { PreloadScene } from './phaser/scenes/preload-scene';
 
 function App() {
+  const [gameSceneLoaded, setGameSceneLoaded] = useState(false);
+  const gameConfig = {
+    type: Phaser.CANVAS,
+    pixelArt: false,
+    scale: {
+      parent: 'phaser-div',
+      width: 1024,
+      height: 576,
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    backgroundColor: '#000000',
+    scene: [PreloadScene],
+  }
 
+  const game = usePhaserGame(gameConfig);
+  const gamescene = gameSceneLoaded ? game?.scene?.keys?.GameScene : null;
+  
   return (
     <>
       Hello Everybutties
+      <div id="phaser-div"/>
     </>
   )
 }
