ceb1fe84dac8c5d189dc9c2981983745104a8115
0xhatsume <0xhatsume@gmail.com>
2024-01-14 00:12:13 +0800

    :lipstick: Update webfont loader code

--- src/phaser/assets/web-font-file-loader.ts
+++ src/phaser/assets/web-font-file-loader.ts
@@ -1,5 +1,5 @@
 import Phaser from 'phaser';
-import * as WebFontLoader from '../lib/webfontloader';
+import WebFont from 'webfontloader';
 
 export class WebFontFileLoader extends Phaser.Loader.File {
   /** @type {string[]} */
@@ -10,27 +10,27 @@ export class WebFontFileLoader extends Phaser.Loader.File {
      * @param {string[]} fontNames The list of font names that will be loaded by the WebFontLoader library.
      *                             The font names need to match the font-family name in the @font-face style declaration.
      */
-    constructor(loader, fontNames) {
+    constructor(loader:Phaser.Loader.LoaderPlugin, fontNames:string[]) {
         super(loader, {
-        type: 'webfont',
-        key: fontNames.toString(),
+            type: 'webfont',
+            key: fontNames.toString(),
         });
 
         this.#fontNames = fontNames;
     }
 
     load() {
-        WebFontLoader.default.load({
-        custom: {
-            families: this.#fontNames,
-        },
-        active: () => {
-            this.loader.nextFile(this, true);
-        },
-        inactive: () => {
-            console.error(`Failed to load custom fonts ${JSON.stringify(this.#fontNames)}`);
-            this.loader.nextFile(this, false);
-        },
+            WebFont.load({
+                custom: {
+                    families: this.#fontNames,
+                },
+            active: () => {
+                this.loader.nextFile(this, true);
+            },
+            inactive: () => {
+                console.error(`Failed to load custom fonts ${JSON.stringify(this.#fontNames)}`);
+                this.loader.nextFile(this, false);
+            },
         });
     }
 }
